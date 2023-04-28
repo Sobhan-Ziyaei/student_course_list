aab9d5eefe2fc26099136284c565a00aa9f5e12b
SohanZiyaei <sobhan.ziaee73@gmail.com>
2023-04-28 13:22:47 +0430

    complete student List

--- lib/screens/student_list.dart
+++ lib/screens/student_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_course_list/data/constants/custom_color.dart';
 
 import '../data/model/student.dart';
 
@@ -21,12 +22,73 @@ class _StudentListState extends State<StudentList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: CustomColor.blackColor,
+        elevation: 0,
+        title: Text(
+          'Student Course List',
+          style: TextStyle(color: CustomColor.greenColor),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: CustomColor.blackColor,
       body: SafeArea(
         child: ListView.builder(
           itemCount: studentList!.length,
           itemBuilder: (context, index) {
-            return Text(studentList![index].firstName);
+            return _getItemList(studentList![index]);
           },
+        ),
+      ),
+    );
+  }
+
+  Widget _getItemList(Student student) {
+    return ListTile(
+      leading: SizedBox(
+        width: 50,
+        child: Center(
+          child: Container(
+            width: 30,
+            height: 30,
+            decoration: BoxDecoration(
+              color: CustomColor.greyColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                student.firstName.characters.first,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+      title: Text(
+        '${student.firstName} ${student.lastName}',
+        style: TextStyle(color: CustomColor.greenColor),
+      ),
+      subtitle: Text(
+        student.course,
+        style: TextStyle(color: CustomColor.greyColor),
+      ),
+      trailing: SizedBox(
+        width: 50,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.bar_chart_rounded,
+              color: CustomColor.greyColor,
+            ),
+            Text(
+              student.score.toString(),
+              style: TextStyle(color: CustomColor.redColor),
+            ),
+          ],
         ),
       ),
     );
